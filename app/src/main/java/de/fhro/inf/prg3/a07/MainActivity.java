8d9069f7d0fec15418bce7adfde234ad524e2dc1
Tobi Rackow <bowser96@gmx.net>
2017-11-22 11:11:46 +0100

    angefangen...

--- app/src/main/java/de/fhro/inf/prg3/a07/MainActivity.java
+++ app/src/main/java/de/fhro/inf/prg3/a07/MainActivity.java
@@ -2,6 +2,10 @@ package de.fhro.inf.prg3.a07;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +16,18 @@ public class MainActivity extends AppCompatActivity {
         // this will inflate the layout from res/layout/activity_main.xml
         setContentView(R.layout.activity_main);
 
-        // add your code here
+
+        Button btn = (Button) findViewById(R.id.button1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ListView lv = (ListView) findViewById(R.id.mealsListView);
+                lv.setAdapter(new ArrayAdapter<>(
+                        MainActivity.this,     // context we're in; typically the activity
+                        R.layout.meal_entry,   // where to find the layout for each item
+                        new String[] {"Hans", "Dampf"} // your data
+                ));
+            }
+        });
     }
 }
